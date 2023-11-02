ecf923102734e971cd34f83b4f052468fe298813
Markus Maal <markus.maal@voco.ee>
2023-11-02 10:56:01 +0200

    Added global color definitions for styling

--- src/screens/app/Notes/styles.js
+++ src/screens/app/Notes/styles.js
@@ -1,14 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+import { colors } from "../../../utils/colors";
+const windowHeight = Dimensions.get('window').height;
 
 export default styles = StyleSheet.create({
     container: {
         padding: 20,
+        backgroundColor: colors.white,
+        minHeight: windowHeight,
     },
 
     text: {
         paddingTop: 20,
         textAlign: 'center',
         fontSize: 25,
-        color: 'midnightblue',
+        color: colors.primary,
     },
 })
---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+// replace with colors from Mi-Fi/Hi-Fi prototype
+export const colors = {
+    white: "#ededed",
+    black: "#000",
+    primary: "#800",
+    secondary: "#080",
+}
